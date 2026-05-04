e41815d374a6f9da308a0722559111739cc4b0ed
Alexey Troshin <alexey.troshin.dev@gmail.com>
2026-05-04 22:05:03 +0300

    Add .config/wezterm/wezterm.lua

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Everblush"
+config.font_dirs = { "/Users/atroshin/Library/Fonts" }
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 16
+config.freetype_load_target = "Light"
+
+config.window_background_opacity = 0.96
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 8,
+  bottom = 8,
+}
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.audible_bell = "Disabled"
+config.initial_cols = 120
+config.initial_rows = 34
+
+return config
