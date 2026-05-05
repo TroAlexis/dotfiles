1cd8bd0b77755bad21e6d78f8980db122c1c2241
Alexey Troshin <alexey.troshin.dev@gmail.com>
2026-05-05 13:17:24 +0300

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -25,8 +25,7 @@ config.audible_bell = "Disabled"
 config.initial_cols = 120
 config.initial_rows = 34
 
--- Treat right option key as Meta
-config.send_composed_key_for_right_alt = false
-config.use_dead_keys = false
+-- Treat right Option as Alt (same as left Option), not as macOS compose key
+config.send_composed_key_when_right_alt_is_pressed = false
 
 return config
