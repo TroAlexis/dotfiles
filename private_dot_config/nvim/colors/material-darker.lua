a8b005c7b12100227cc1e89692617fcef7ab3c41
Alexey Troshin <alexey.troshin.dev@gmail.com>
2026-09-19 17:43:13 +0300

    Add .config/nvim/colors/material-darker.lua Add .config/nvim/lua/lualine/themes/material-darker.lua Add .config/nvim/lua/material-darker.lua Add .config/nvim/tests/theme_snapshot.lua

---
+++ private_dot_config/nvim/colors/material-darker.lua
@@ -0,0 +1,1 @@
+require("material-darker").load()
---
+++ private_dot_config/nvim/lua/lualine/themes/material-darker.lua
@@ -0,0 +1,2 @@
+-- Keep lualine's automatic theme selection identical to the Catppuccin backend.
+return require("catppuccin.utils.lualine")("macchiato")
